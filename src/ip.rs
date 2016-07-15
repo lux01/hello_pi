a7f4c7d32c5c31e6cd8c30616edb04859824d864
Willam Woodhead <william.woodhead@lux01.co.uk>
2016-07-15 20:40:54 +0100

    Uses libc to get IP addresses now

--- src/ip.rs
+++ src/ip.rs
@@ -1,10 +1,19 @@
 use libc;
-use std::collections::HashMap;
-
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
-pub fn get_if_addresses() -> Option<HashMap<String, IpAddr>>{
-    let mut addrs_map = HashMap::new();
+#[derive(Debug)]
+pub struct Interface {
+    pub name: String,
+    pub addr: IpAddr
+}
+
+#[derive(Debug)]
+pub enum InterfaceError {
+    FfiError
+}
+
+pub fn get_if_addresses() -> Result<Vec<Interface>, InterfaceError>{
+    let mut addrs_vec = Vec::new();
 
     unsafe {
         use std::ptr::{null_mut};
@@ -16,14 +25,16 @@ pub fn get_if_addresses() -> Option<HashMap<String, IpAddr>>{
         let mut ifaddrs: *mut libc::ifaddrs = null_mut();
         let mut ifa: *mut libc::ifaddrs;
 
-        
+
+        // Build the linked list of interfaces, return None on error.
+        // TODO: Handle this error with better accuracy.
         if libc::getifaddrs(&mut ifaddrs) == -1 {
-            return None;
+            return Err(InterfaceError::FfiError);
         }
 
         // Store the head for later so we can free the entire list
         ifa = ifaddrs;
-
+        
         while !ifa.is_null() {
             if !(*ifa).ifa_addr.is_null() {
                 use std::ffi::CStr;
@@ -32,18 +43,27 @@ pub fn get_if_addresses() -> Option<HashMap<String, IpAddr>>{
                 let name = CStr::from_ptr((*ifa).ifa_name)
                     .to_string_lossy()
                     .into_owned();
-
+                
+                // The ifa_addr member can be one of many different sockaddr_X
+                // structs. We are only interested in AF_INET (IPv4) and AF_INET6 (IPv6)
+                // ones. They return the addresses in various formats, so we have to do
+                // some bit juggling to get them into a form suitable for Ipv4Addr and
+                // Ipv6Addr respectively.
                 if family == libc::AF_INET {
-                    // Read the IP address as a u32 in Network Order (big endian)
-                    let addr = (*((*ifa).ifa_addr as *mut libc::sockaddr_in)).sin_addr
+                    let addr = (*((*ifa).ifa_addr as *mut libc::sockaddr_in))
+                        .sin_addr
                         .s_addr
-                        .to_be();
+                        .to_be(); // Ensure the bytes are big endian
+                    
                     let a = ((addr >> 24) & 0xFFu32) as u8;
                     let b = ((addr >> 16) & 0xFFu32) as u8;
                     let c = ((addr >>  8) & 0xFFu32) as u8;
                     let d = ((addr >>  0) & 0xFFu32) as u8;
 
-                    addrs_map.insert(name, IpAddr::V4(Ipv4Addr::new(a, b, c, d)));
+                    addrs_vec.push(Interface {
+                        name: name,
+                        addr: IpAddr::V4(Ipv4Addr::new(a, b, c, d)),
+                    });
                 } else if family == libc::AF_INET6 {
                     let addr = (*((*ifa).ifa_addr as *mut libc::sockaddr_in6))
                         .sin6_addr
@@ -58,17 +78,19 @@ pub fn get_if_addresses() -> Option<HashMap<String, IpAddr>>{
                     let g = ((addr[12] as u16) << 8) | (addr[13] as u16);
                     let h = ((addr[14] as u16) << 8) | (addr[15] as u16);
 
-                    addrs_map.insert(name, IpAddr::V6(Ipv6Addr::new(a, b, c, d, e, f, g, h)));
-                    
+                    addrs_vec.push(Interface {
+                        name: name,
+                        addr: IpAddr::V6(Ipv6Addr::new(a, b, c, d, e, f, g, h))
+                    });
                 }
             }
-
+            // Advance to the next interface in the list.
             ifa = (*ifa).ifa_next;
         }
 
-
+        // Free the linked list that we requested.
         libc::freeifaddrs(ifaddrs);
 
-        Some(addrs_map)
+        Ok(addrs_vec)
     }
 }
